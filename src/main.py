5660ec2b0f822027f5cff070342ed8e8898b884e
Fernando Perry <fernandoperry1234@gmail.com>
2025-12-07 13:02:39 +0700

    start working on backend

--- src/main.py
+++ src/main.py
@@ -2,35 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# --------------------
+# -- Frontend route --
+# --------------------
 # Home Route
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 # Camera Route
 @app.route('/camera', methods=['GET', 'POST'])
 def camera():
     return render_template('camera.html')
 
-
 # About Route
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-
 # Register Route
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+# -------------------
+# -- Backend Route --
+# -------------------
+@app.route('/be/login', methods=['POST'])
+def be_login():
+    pass
 
 
 if __name__ == '__main__':
