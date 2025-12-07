20a3be48ad8b09a5fc1f4dfd09fe3fd8a0905616
Fernando Perry <fernandoperry1234@gmail.com>
2025-12-08 01:40:41 +0700

    add the api to upload

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, session, redirect, request, jsonify
 from connection import Database
 import utils
+import os
 import re
+import base64
+import time
 
 app = Flask(__name__)
 app.secret_key = "INI KUNCI RAHASIA YANG TIDAK RAHASIA C4F3B4BE600DF00D"
@@ -93,6 +96,7 @@ def be_login():
         if utils.verify_password(row["password"], password):
             session["logged_in"] = True
             session["user"] = row["name"]
+            session["id"] = row["id"]
             return jsonify({
                 "status": 1,
                 "message": {
@@ -155,6 +159,66 @@ def be_logout():
     return jsonify({
         "status": 1,
         "message": "Logged out"
+    })
+
+@app.rout('/be/add-whitelist', methods=["POST"])
+def be_add_whitelist():
+    logged_in = session.get("logged_in")
+    if not logged_in:
+        return jsonify({
+            "status": 0,
+            "message": "Need to be logged-in first."
+        })
+    userid = session.get("id")
+    targetdir = f"../{id}_whitelist"
+    os.mkdirs(targetdir, exist_ok=True)
+    if not request.is_json:
+        return jsonify({
+            "status": 0,
+            "message": "Body need to be at JSON format."
+        })
+    try:
+        data = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "status": 0,
+            "message": f"error parsing the body because: {e}."
+        })
+
+    imgdata = data.get("image")
+    if len(imgdata) == 0:
+        return jsonify({
+            "status": 0,
+            "message": "Empty data is not a valid data."
+        })
+
+    data_pattern = re.compile(r'data:image/.*?;base64,(.*)')
+    match = data_pattern.match(imgdata)
+    if match:
+        encoded_data = match.group(1)
+        extension_match = re.search(r'data:image/(\w+);', imgdata)
+        extension = extension_match.group(1) if extension_match else 'png'
+    else:
+        encoded_data = imgdata
+        extension = 'png' # Default extension if header is missing
+
+    try:
+        image_bytes = base64.b64decode(encoded_data)
+    except base64.binascii.Error as e:
+        print(f"Error decoding Base64 data: {e}")
+        return jsonify({
+            "status": 0,
+            "message": "Failed to decode the base64 string."
+        })
+
+    current = time.time()
+    filename = f"{id}_{int(time)}.{extension}"
+    filepath = os.path.join(targetdir, filename)
+    with open(filepath, 'wb') as f:
+        f.write(image_bytes)
+    return jsonify({
+        "status": 1,
+        "message": "File saved!"
     })
 
 if __name__ == '__main__':
